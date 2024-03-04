3d342e606b4f399c5d65ba98d117618d94d0e6d6
NelsonCC <nelsoncc1812@gmail.com>
2024-03-04 21:17:23 +0000

    added Users seed
    modified scripts

---
+++ server/bin/seeds/Users.seed.js
@@ -0,0 +1,24 @@
+const User = require('../../schemas/User.schema')
+const Users = require('../../models/Users.model')
+
+const adjective = ["Excited", "Anxious", "Overweight", "Demonic", "Jumpy", "Misunderstood", "Squashed", "Gargantuan", "Broad", "Crooked", "Curved", "Deep", "Even", "Excited", "Anxious", "Overweight", "Demonic", "Jumpy", "Misunderstood", "Squashed", "Gargantuan", "Broad", "Crooked", "Curved", "Deep", "Even", "Flat", "Hilly", "Jagged", "Round", "Shallow", "Square", "Steep", "Straight", "Thick", "Thin", "Cooing", "Deafening", "Faint", "Harsh", "High-pitched", "Hissing", "Hushed", "Husky", "Loud", "Melodic", "Moaning", "Mute", "Noisy", "Purring", "Quiet", "Raspy", "Screeching", "Shrill", "Silent", "Soft", "Squeaky", "Squealing", "Thundering", "Voiceless", "Whispering"]
+const object = ["Taco", "Operating System", "Sphere", "Watermelon", "Cheeseburger", "Apple Pie", "Spider", "Dragon", "Remote Control", "Soda", "Barbie Doll", "Watch", "Purple Pen", "Dollar Bill", "Stuffed Animal", "Hair Clip", "Sunglasses", "T-shirt", "Purse", "Towel", "Hat", "Camera", "Hand Sanitizer Bottle", "Photo", "Dog Bone", "Hair Brush", "Birthday Card"]
+
+
+for (let i = 0; i < 20; i++) {
+    do {
+        const adj = adjective[Math.floor(Math.random() * adjective.length)]
+        const obj = object[Math.floor(Math.random() * object.length)]
+
+    } while (!Users.add({
+        name: `${adj} ${obj}`,
+        password: 'pass',
+        email: `${adj}${obj}@domain.com`
+    }))
+
+}
+
+
+console.log('+--------------------+')
+console.log('| Users DB populated |')
+console.log('+--------------------+')
