4259cabf8dad31ffd04b2206e1f654acac9d3d0f
Ramón Valls Martin <ravama00@hotmail.com>
2024-03-05 12:22:32 +0000

    creación directorio firebase

---
+++ server/firebase/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyA0fOXi_EkLYgoVWl-wRw3LgGwgfLkixSg",
+    authDomain: "ps-uwutracker.firebaseapp.com",
+    projectId: "ps-uwutracker",
+    storageBucket: "ps-uwutracker.appspot.com",
+    messagingSenderId: "30268734424",
+    appId: "1:30268734424:web:cf2c28ea2d44e4e38b90ad",
+    measurementId: "G-YY3Y9EL1JX"
+};
