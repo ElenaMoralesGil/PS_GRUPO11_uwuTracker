894b0f53211f5733197710603c0fc1e235f319b8
NelsonCC <nelsoncc1812@gmail.com>
2024-03-06 16:00:08 +0000

    client env vars dispatch works

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,6 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
+import { __env } from '../environments/env'
+process.env = __env
+
 export const appConfig: ApplicationConfig = {
   providers: [ provideRouter(routes), provideClientHydration() ]
 };
