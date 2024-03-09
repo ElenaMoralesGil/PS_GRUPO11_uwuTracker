fb8229ae37d88f9c4a14819f62350dac630e9973
NelsonCC <nelsoncc1812@gmail.com>
2024-03-09 01:23:51 +0000

    content, findByName all done

--- server/services/jikan/JikanContent.service.js
+++ server/services/jikan/JikanContent.service.js
@@ -4,6 +4,11 @@ class JikanService {
     }
 
     findById = (id) => fetch(`${this.path}/${id}`).then(res => res.json()).then(res => res.data)
+    findByName = name => fetch(`${this.path}?q="${name}"`).then(res => res.json()).then(res => {
+        console.log("FETCH: ", `${this.path}?q="${name}"`)
+        console.log(res)
+        return res.data
+    })
 }
 
 module.exports = require('../../bin/Singleton')(new JikanService())
