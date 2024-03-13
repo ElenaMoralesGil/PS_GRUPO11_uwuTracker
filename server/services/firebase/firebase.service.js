681ac6c6768ffc8355804a39f3e79d05fbd871f4
NelsonCC <nelsoncc1812@gmail.com>
2024-03-13 23:52:18 +0000

    added firebase admin
    set session persistence to none

--- server/services/firebase/firebase.service.js
+++ server/services/firebase/firebase.service.js
@@ -3,22 +3,32 @@ const { getFirestore } = require('firebase/firestore/lite')
 const { getStorage } = require('firebase/storage')
 const { getAuth } = require('firebase/auth')
 
+const admin = require('firebase-admin')
+
 
 class FirebaseService {
 
     #app
+    #admin
+
     #db
     #storage
     #auth
 
     constructor() {
         this.#app = initializeApp(JSON.parse(process.env.FIRE_API))
+        this.#admin = admin.initializeApp({ credential: admin.credential.cert(JSON.parse(process.env.FIRE_ADMIN)) })
+
         this.#db = getFirestore(this.#app)
         this.#storage = getStorage(this.#app)
+
         this.#auth = getAuth(this.#app)
+        this.#auth.setPersistence("NONE")
     }
 
     get app() { return this.#app }
+    get admin() { return this.#admin }
+
     get db() { return this.#db }
     get storage() { return this.#storage }
     get auth() { return this.#auth }
