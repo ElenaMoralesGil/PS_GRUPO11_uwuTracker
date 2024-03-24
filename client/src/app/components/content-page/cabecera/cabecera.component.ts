5f9b77844f8a57d0094c657aa489ba4c0206dd00
Ramón Valls Martin <ravama00@hotmail.com>
2024-03-24 11:23:20 +0000

    cambios en listas de cabecera

--- client/src/app/components/content-page/cabecera/cabecera.component.ts
+++ client/src/app/components/content-page/cabecera/cabecera.component.ts
@@ -1,20 +1,47 @@
 import { Component, Input, input } from '@angular/core';
 import { ApiContentService } from '../../../services/api-content.service';
 import { ActivatedRoute } from '@angular/router';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-cabecera',
   standalone: true,
-  imports: [],
+  imports: [NgFor, CommonModule],
   templateUrl: './cabecera.component.html',
   styleUrl: './cabecera.component.css'
 })
+
 export class CabeceraComponent {
   
-  @Input() title: string[] = [];
-  @Input() description: string[] = [];
-  @Input() img: string[] = [];
+  @Input() title: string | undefined;
+  @Input() description: string | undefined;
+  @Input() img: string | undefined;
 
+  ratingSelected: boolean = false;
+  ratingOptions: number[] = [0, 1, 2, 3, 4, 5];
+
+  otherSelected: boolean = false;
+  addList: string[] = ['Completed', 'Pending', 'Not wanted'];
+
+  selectedRating: string = "";
+  selectedList: string = '';
+
+
+  getRatings(): number[] {
+    return this.ratingOptions
+  }
+  getLists(): string[] {
+    return this.addList
+  }
+
+  // clics en los botones
+  scrollValorar() {
+      this.ratingSelected = !this.ratingSelected;
+  }
+
+  scrollListas() {
+      this.otherSelected = !this.otherSelected;
+  }
 
     
 }
