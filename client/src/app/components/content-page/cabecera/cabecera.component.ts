576ee9d623fbd9287f35af4acda847d7cb2cf7da
Ramón Valls Martin <ravama00@hotmail.com>
2024-03-26 19:42:31 +0000

    modificación dropdowns

--- client/src/app/components/content-page/cabecera/cabecera.component.ts
+++ client/src/app/components/content-page/cabecera/cabecera.component.ts
@@ -1,31 +1,36 @@
-import { Component, Input, input } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, input } from '@angular/core';
 import { ApiContentService } from '../../../services/api-content.service';
 import { ActivatedRoute } from '@angular/router';
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-cabecera',
   standalone: true,
-  imports: [NgFor, CommonModule],
+  imports: [NgFor, CommonModule, NgIf],
   templateUrl: './cabecera.component.html',
   styleUrl: './cabecera.component.css'
 })
 
 export class CabeceraComponent {
   
+  constructor(private cdr: ChangeDetectorRef) { }
+
   @Input() title: string | undefined;
   @Input() description: string | undefined;
   @Input() img: string | undefined;
+  @Input() rating: number | undefined;
+
+
+  selectedRate: number | undefined;
+  selectedList: string | undefined;
+
+
 
   ratingSelected: boolean = false;
   ratingOptions: number[] = [0, 1, 2, 3, 4, 5];
 
-  otherSelected: boolean = false;
+  listSelected: boolean = false;
   addList: string[] = ['Completed', 'Pending', 'Not wanted'];
-
-  selectedRating: string = "";
-  selectedList: string = '';
-
 
   getRatings(): number[] {
     return this.ratingOptions
@@ -34,13 +39,29 @@ export class CabeceraComponent {
     return this.addList
   }
 
+  isRatingSelected(option: number): boolean {
+    return this.selectedRate === option;
+  }
+  ratingSelection(option: number) {
+    this.selectedRate = option;
+    this.cdr.detectChanges();
+  }
+
+  isListSelected(option: string): boolean {
+    return this.selectedList === option;
+  }
+  listSelection(option: string) {
+    this.selectedList = option;
+    this.cdr.detectChanges();
+  }
+
   // clics en los botones
   scrollValorar() {
       this.ratingSelected = !this.ratingSelected;
   }
 
   scrollListas() {
-      this.otherSelected = !this.otherSelected;
+      this.listSelected = !this.listSelected;
   }
 
     
