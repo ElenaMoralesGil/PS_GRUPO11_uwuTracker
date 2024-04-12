bad9887c2719b2d28ab8d5be2fefd7e28be19a9a
Ramón Valls Martin <ravama00@hotmail.com>
2024-04-12 13:39:54 +0100

    cambios en home para mostrar animes

--- client/src/app/components/home/animelist/animelist.component.ts
+++ client/src/app/components/home/animelist/animelist.component.ts
@@ -1,14 +1,17 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { AnimecardComponent } from '../../sharedComponents/animecard/animecard.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-animelist',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [AnimecardComponent],
+  imports: [AnimecardComponent, NgFor],
   templateUrl: './animelist.component.html',
   styleUrl: './animelist.component.css'
 })
 export class AnimelistComponent {
-  descriptor = input("Anime Row");
+  //descriptor = input("Anime Row");
+  descriptor = ["Animes Populares", "Animes temporada actual", "Recomendaciones aleatorias"];
+
 }
