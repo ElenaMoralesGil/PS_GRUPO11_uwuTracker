da62023c321b158139f48383b90398b0d9ba748a
NelsonCC <nelsoncc1812@gmail.com>
2024-04-20 20:43:28 +0100

    Score utils
    * jikanScoreNormalization
    * calcScore
    * calcWeithedScore

--- server/bin/utils/score.utils.js
+++ server/bin/utils/score.utils.js
@@ -1,13 +1,18 @@
 
 const jikanScoreNormalization = score => 5 / 9 * score - 5 / 9
 
-
-const calcScore = (ourScore, scoreCount, jikanScore) => {
+const calcScore = ({ ourScore, scoreCount, jikanScore }) => {
     const USER_THRESHOLD = 100
 
     return scoreCount >= USER_THRESHOLD ? ourScore
         : (scoreCount / USER_THRESHOLD) * ourScore + (1 - (scoreCount / USER_THRESHOLD)) * jikanScore
 }
 
+const calcWeithedScore = ({ prevUserScore = null, userScore, score, scoreCount }) => {
+    const percent = (scoreCount - 1) / scoreCount
 
-module.exports = { jikanScoreNormalization, calcScore }
+    return prevUserScore == null ? score * percent + (1 - percent) * userScore
+        : score - (1 - percent) * prevUserScore + (1 - percent) * userScore
+}
+
+module.exports = { jikanScoreNormalization, calcScore, calcWeithedScore }
