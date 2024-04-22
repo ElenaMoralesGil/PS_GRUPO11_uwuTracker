c86a72e32792f4d3e97a49c45c63eb97f894b1b7
NelsonCC <nelsoncc1812@gmail.com>
2024-04-22 12:50:38 +0100

    now hero redirects to content

--- client/src/app/components/home/hero/hero.component.ts
+++ client/src/app/components/home/hero/hero.component.ts
@@ -1,15 +1,17 @@
 import { ChangeDetectionStrategy, Component, Input, InputSignal, OnInit, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ApiContentService } from '../../../services/api-content.service';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [ RouterLink ],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
 export class HeroComponent implements OnInit {
 
+  id: string = ""
   title: string = ""
   description: string = ""
   imageSource: string = ""
@@ -20,6 +22,7 @@ export class HeroComponent implements OnInit {
 
     this.Contents.find({}, { orderBy: 'likes', orderByDir: 'desc', limit: 1 }).then(contents => {
       const content = contents[ 0 ];
+      this.id = content.id
       this.title = content.title
       this.description = content.synopsis
       this.imageSource = content.coverImg
