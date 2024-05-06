c456062188aa3bd9dc93f50a9dd6636b4bb3772f
ElenaMoralesGil <elena.morales104@alu.ulpc.es>
2024-05-06 10:59:41 +0100

    improved header now with mobile

--- client/src/app/components/header/sign-in/sign-in.component.ts
+++ client/src/app/components/header/sign-in/sign-in.component.ts
@@ -1,34 +1,54 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import {Component, Output, EventEmitter} from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { ActivatedRoute } from '@angular/router';
-import { UsersService } from '../../../services/users.service';
-import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UsersService } from '../../../services/users.service'
+import {FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule, AbstractControl} from '@angular/forms';
+import {NgClass, NgIf} from "@angular/common";
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [ FormsModule, ReactiveFormsModule ],
+  imports: [FormsModule, ReactiveFormsModule, NgClass, NgIf],
   templateUrl: './sign-in.component.html',
   styleUrl: '../sign-up/sign-up.component.css',
 })
 export class SignInComponent {
   @Output() signIn = new EventEmitter();
-
+  @Output() signUp = new EventEmitter();
+  errorLogin: boolean = false;
   form: FormGroup;
 
   constructor(private formBuilder: FormBuilder, private Users: UsersService, private router: ActivatedRoute, private Auth: AuthService) {
     this.form = this.formBuilder.group({
       username: [ '', [ Validators.required, Validators.minLength(2) ] ],
-      password: [ '', [ Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9\s]{8,}$/) ] ]
+      password: [ '', [ Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/u
+      ) ] ]
+    }, {
+      validators: this.login
     });
   }
 
-  signUpClicked() {
+  signInClicked() {
     this.signIn.emit();
   }
 
   async login() {
-    const { username, password } = this.form.value
-    await this.Auth.login({ username, password })
-    this.signUpClicked()
+    const { username, password } = this.form.value;
+    try {
+      const response = await this.Auth.login({ username, password });
+      console.log(response);
+      if (response !== null) {
+        this.signInClicked();
+        alert('SignIn successful');
+      } else {
+        this.errorLogin = true;
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+      this.errorLogin = true;
+    }
+  }
+
+  redirectSignUp() {
+    this.signUp.emit()
   }
 }
